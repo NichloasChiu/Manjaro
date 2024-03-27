60ef82637de6afcb8a5d07494efc017bcdb02211
NichloasChiu <NichloasChiu@outlook.com>
2024-03-27 20:15:59 +0800

    update clean.sh Add deletion cache

--- clean.sh
+++ clean.sh
@@ -7,4 +7,37 @@
 ##########################################################################################################
 sudo pacman -Sc --noconfirm
 sudo pacman -Scc --noconfirm
-sudo journalctl --vacuum-size=100M
+sudo journalctl --vacuum-size=50M
+
+function search_soft() {
+	clear
+	echo "Query personal data left"
+	find ~/.config/ -type d -name "*$1*"
+	echo "---------------------------------------------------------------------------------------------------------------------------"
+	echo "Query leftover system data"
+	echo "---------------------------------------------------------------------------------------------------------------------------"
+	sudo find /usr/share -type d -name "*$1*"
+	sudo find /usr/lib/ -type d -name "*$1*"
+	echo "Query other related files"
+	echo "---------------------------------------------------------------------------------------------------------------------------"
+	find ~/.cache/ -type d -name "*$1*"
+	find ~/.local/share/ -type d -name "*$1*"
+}
+
+while true; do
+	search_soft $1
+	read -p "Enter the folder path to be deleted (enter \"n\" to stop)" path
+	if [ "$path" = "n" ]; then
+		echo "Deleted."
+		break
+	fi
+
+	if [ -d "$path" ]; then
+		rm -rf "$path"
+		echo "Deleted folders: $path"
+		clear
+
+	else
+		echo "The folder does not exist：$path"
+	fi
+done
